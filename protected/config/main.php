36b9b52628b051215590f5fcdc9a1834656fe53e
hammadhk <hammadbinhassan@gmail.com>
2015-05-03 20:34:51 +0500

    nginx configuration added in setup other directories also added via .gitignore

--- protected/config/main.php
+++ protected/config/main.php
@@ -38,6 +38,7 @@ return array(
 
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
